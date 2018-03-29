eedf303b6bf9d350fb6bc68611f4fd9e64dc61a2
CorentinN <noirot.co@gmail.com>
2018-03-29 10:03:39 +0200

    testgitignore

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
+import './style/style.scss';
+
+
+ReactDOM.render(<p>React Boilerplate</p>, document.getElementById('app'));
+
